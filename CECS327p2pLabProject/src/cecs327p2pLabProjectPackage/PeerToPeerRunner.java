e2530ae47d0c3d5f0dc171671242672bff3db11d
tyler <tylerdvanl@gmail.com>
2021-07-30 13:31:52 -0700

    I'm pushing this to make sure I have everything set up properly - Tyler

---
+++ CECS327p2pLabProject/src/cecs327p2pLabProjectPackage/PeerToPeerRunner.java
@@ -0,0 +1,12 @@
+package cecs327p2pLabProjectPackage;
+
+public class PeerToPeerRunner {
+
+	//Im pushing this to make sure I have everything set up properly - Tyler
+	public static void main(String[] args) 
+	{
+
+
+	}
+
+}
