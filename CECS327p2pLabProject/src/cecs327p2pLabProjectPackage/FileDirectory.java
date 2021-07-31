a3d3b1e835f7fee698bc76b1383a4f7f38cafb39
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2021-07-31 14:36:22 -0700

    Skeleton code for FileDirectory Sync

---
+++ CECS327p2pLabProject/src/cecs327p2pLabProjectPackage/FileDirectory.java
@@ -0,0 +1,14 @@
+/**
+ * FileDirectory Class intended to sync file directories together.
+ */
+
+public class FileDirectory 
+{
+	private File[] FileList;
+	
+	public FileDirectory()
+	{
+		FileList = null;
+	}
+
+}
