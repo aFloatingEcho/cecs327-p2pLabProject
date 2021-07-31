ae215e3ca4a08f0e2418b8c28caa10fda01d71c6
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2021-07-31 14:39:55 -0700

    Relocate directory for dev work and added imports

--- src/FileDirectory.java
+++ src/FileDirectory.java
@@ -2,6 +2,9 @@
  * FileDirectory Class intended to sync file directories together.
  */
 
+import java.util.ArrayList; // Import Java ArrayList in order to handle file lists.
+import java.io.*; // Import Java library to handle directories
+
 public class FileDirectory 
 {
 	private File[] FileList;
