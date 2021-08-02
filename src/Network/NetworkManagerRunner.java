cb3cf771e49cab64b7f189578fff87fee7f8118c
bobdidley <james.austin@student.csulb.edu>
2021-08-01 22:10:10 -0700

    IP Addresses are read in NetworkManager

---
+++ src/Network/NetworkManagerRunner.java
@@ -0,0 +1,8 @@
+package Network;
+
+public class NetworkManagerRunner {
+    public static void main(String[] args) {
+        NetworkManager nm = new NetworkManager();
+        nm.getLocalIPAddresses();
+    }
+}
