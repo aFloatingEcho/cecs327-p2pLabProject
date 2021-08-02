add46a8226fbb271959c6888f2c8c7645f98bc1a
bobdidley <james.austin@student.csulb.edu>
2021-08-02 14:46:22 -0700

    NetworkManager returns complete IP address list

--- src/Network/NetworkManager.java
+++ src/Network/NetworkManager.java
@@ -8,7 +8,11 @@ import java.net.InetAddress;
 // NOTE: this class is specifically used for transferring files to peers over UDP
 import java.net.MulticastSocket;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class NetworkManager {
 	
@@ -44,24 +48,34 @@ public class NetworkManager {
 	 * Returns list of all local IP addresses
 	 * @return List<String>
 	 */
-	public List<String> getLocalIPAddresses() {
+	public List<String> getLocalIPAddresses() throws InterruptedException {
+		List<String> localIPs = new ArrayList<>();
 		final byte[] ip = INET.getAddress();
-		for(int i = 1; i < 10; ++i) {
-			try {
-                ip[3] = (byte) i;   // changes byte address to iterate through local addresses
-                InetAddress address = InetAddress.getByAddress(ip);   // instantiates an address
-                String localIP = address.toString().substring(1);   // instantiates the string value of the address
-                if (address.isReachable(5000)) {
-                    System.out.println(localIP + " is on the network");   
-                } else {
-                    System.out.println("Not Reachable: " + localIP);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+		ExecutorService es = Executors.newFixedThreadPool(255);   // 255 = threads per network address
+		for(int i = 1; i < 255; ++i) {
+			int finalI = i;   // need temp variable for inside the thread b/c can't modify the iterating variable
+			es.submit(new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						ip[3] = (byte) finalI;   // changes byte address to iterate through local addresses
+						InetAddress address = InetAddress.getByAddress(ip);   // instantiates an address
+						String localIP = address.toString().substring(1);   // instantiates the string value of the address
+						if (address.isReachable(5000)) {
+							localIPs.add(localIP);
+							System.out.println(localIP + " is on the network");
+						}
+					} catch (Exception e) {
+//						e.printStackTrace();
+					}
+				}
+			}));
 		}
-		
-		return null;
+
+		es.awaitTermination(10000L, TimeUnit.MILLISECONDS);   // timeout may miss some thread completions, but avoids waiting forever
+//		es.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);   // waits forever
+		es.shutdown();
+		return localIPs;
 	}
 	
 }
--- src/Network/NetworkManagerRunner.java
+++ src/Network/NetworkManagerRunner.java
@@ -1,8 +1,12 @@
 package Network;
 
+import java.util.List;
+
 public class NetworkManagerRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         NetworkManager nm = new NetworkManager();
-        nm.getLocalIPAddresses();
+//        List<String> ipLst = nm.getLocalIPAddresses();
+        System.out.println(nm.getLocalIPAddresses().size());
+//        System.out.println(ipLst);
     }
 }
