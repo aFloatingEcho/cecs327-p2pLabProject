00ef87458d1f7421e5dfef506b860db13e7acda0
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2021-08-02 16:56:23 -0700

    updated network manager to handle multiple network devices

--- src/Network/NetworkManager.java
+++ src/Network/NetworkManager.java
@@ -2,6 +2,7 @@ package Network;
 
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.net.InetAddress;
 
@@ -9,48 +10,53 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.net.NetworkInterface;
 
 public class NetworkManager {
 	
-	private InetAddress INET;
-	private String HOST;
+	private Enumeration<NetworkInterface> INET;
 	
 	/**
 	 * Default Constructor for this machine's host name and address
 	 */
 	public NetworkManager() {
 		try {
-			HOST = InetAddress.getLocalHost().getHostName();
-			INET = InetAddress.getByName(HOST);
-		} catch(UnknownHostException e) {
+			this.INET = NetworkInterface.getNetworkInterfaces();
+		} catch (SocketException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	/**
-	 * Overloaded Constructor for specific host name
-	 * @param host
+	 * Function that cycles through all itnerfaces of a System.
+	 * @return
+	 * @throws InterruptedException
 	 */
-	public NetworkManager(String host) {
-		this.HOST = host;
-		try {
-			INET = InetAddress.getByName(HOST);
-		} catch(UnknownHostException e) {
-			e.printStackTrace();
+	public List<String> getAllLocalIPAddresses() throws InterruptedException{
+		List<String> allLocalIPs = new ArrayList<>();
+		for(NetworkInterface eachAddress : Collections.list(this.INET)) {
+			Enumeration<InetAddress> NetworkAddresses = eachAddress.getInetAddresses();
+			System.out.println("Finding All Addresses on " + eachAddress.getDisplayName() + " on " + eachAddress);
+			for(InetAddress networkAddress : Collections.list(NetworkAddresses)) {
+				allLocalIPs.addAll(this.getLocalIPAddresses(networkAddress));
+			}
 		}
+		return allLocalIPs;
 	}
 	
 	/**
-	 * Returns list of all local IP addresses
+	 * Returns list of all local IP addresses for a particular interface
 	 * @return List<String>
 	 */
-	public List<String> getLocalIPAddresses() throws InterruptedException {
+	public List<String> getLocalIPAddresses(InetAddress networkAddress) throws InterruptedException {
 		List<String> localIPs = new ArrayList<>();
-		final byte[] ip = INET.getAddress();
+		final byte[] ip = networkAddress.getAddress();
 		ExecutorService es = Executors.newFixedThreadPool(255);   // 255 = threads per network address
 		for(int i = 1; i < 255; ++i) {
 			int finalI = i;   // need temp variable for inside the thread b/c can't modify the iterating variable
