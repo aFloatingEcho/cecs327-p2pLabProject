18802fa24c235917cc0004929b4f8640566de895
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2021-08-04 16:18:19 -0700

    testing modifications

--- src/FileDirectoryRunner.java
+++ src/FileDirectoryRunner.java
@@ -6,10 +6,21 @@ import java.nio.file.Files;
 public class FileDirectoryRunner {
 
 	public static void main(String[] args) {
-		FileDirectory syncFolder = new FileDirectory("sync");
+		FileDirectory syncFolder = new FileDirectory("sync\\");
 		ArrayList<String> fileList = syncFolder.getFileList();
-		for(String files: fileList) {
-			System.out.println(files);
+		ArrayList<File> actualFiles = syncFolder.getActualFileList();
+		for(File files: actualFiles) {
+			try {
+				System.out.println(files.getCanonicalPath());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for(String files: syncFolder.getFileList()) {
+			if(syncFolder.doesFileExist(files)){
+				System.out.println("true");
+			}
 		}
 	}
 
