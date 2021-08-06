56f89902f3a188a2bf7324860e2de05c38549134
tylerdvanl <49462625+tylerdvanl@users.noreply.github.com>
2021-08-06 00:17:40 -0700

    Chat Functionality (TODO: Threading)
    Threads are required for this solution, which requires reading, but i'm tired.

---
+++ src/chatThings/Server.java
@@ -0,0 +1,58 @@
+package chatThings;
+
+import java.io.*;
+import java.net.*;
+
+public class Server implements Runnable
+{
+	 ServerSocket serverSocket;
+	 Socket clientSocket;
+	 PrintWriter out;
+	 BufferedReader in;
+	  
+	 public Server(int portNum) throws IOException
+	 {
+		 serverSocket = new ServerSocket(portNum);
+	 }
+	 
+	 public void listenAndProvideService() throws IOException
+	 {
+		 //Accept an incoming connection
+		 clientSocket = serverSocket.accept();
+		 out = new PrintWriter(clientSocket.getOutputStream(), true);
+		 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		 
+		 //Process input and output
+		 String input;
+		 String output;
+		 
+		 while((input = in.readLine()) != null)
+		 {
+			 output = "Server received: " + input;
+			 out.println(output);
+			 
+		 }
+		 
+	 }
+
+	@Override
+	public void run() 
+	{
+		try 
+		{
+			this.listenAndProvideService();
+		}
+		catch (IOException e) 
+		{
+			System.out.println("Listen thread broke :/");
+		}
+		
+	}
+	 
+
+	
+	
+	
+	
+	
+}
