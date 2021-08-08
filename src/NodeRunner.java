ff8094aad8efde113beee69abaef740d786ba134
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2021-08-07 18:24:11 -0700

    test the multithreading

--- src/NodeRunner.java
+++ src/NodeRunner.java
@@ -2,6 +2,7 @@ public class NodeRunner {
     public static void main(String[] args) {
         Node nTest = new Node();
         // initializes communication between a Client and a Server
-        nTest.connect("PLACEHOLDER", 5555);
+        nTest.runNode();
+        nTest.leaveNetwork();
     }
 }
